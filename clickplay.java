2f566f97afdc4203ef9f113c06876bb9c2ec969d
Nicole <143809859+nicole0625@users.noreply.github.com>
2025-05-29 09:50:05 -0400

    Update clickplay.java

--- clickplay.java
+++ clickplay.java
@@ -7,6 +7,7 @@ public class clickplay{
 		String strSelectedTheme;
 		con.println("Please enter your name:");
 		strname = con.readLine();
+		strSelectedTheme = "";
 		con.println("Hi, "+strname+"! Type in the theme you wish to play:");
 		if(!strSelectedTheme.equalsIgnoreCase("foods") || !strSelectedTheme.equalsIgnoreCase("musicals") || !strSelectedTheme.equalsIgnoreCase("books") || !strSelectedTheme.equalsIgnoreCase("flowers") || !strSelectedTheme.equalsIgnoreCase("CRK")){
 			con.println("Foods   ||   Musicals   ||   Books   ||   Flowers   ||   CRK");
@@ -16,13 +17,13 @@ public class clickplay{
 		}
 		
 		//creating array for selected theme
-		String strword[];
+		String strword[][];
 		String strread;
-		int intrand[];
 		int intthemenum;
 		intthemenum = -1;
 		int intcount;
 		intcount = -1;
+		
 		TextInputFile theme = new TextInputFile(strSelectedTheme+".txt");
 		while(theme.eof()==false){
 			//getting the number of words of the selected theme
@@ -31,15 +32,24 @@ public class clickplay{
 		}
 		theme.close();
 		TextInputFile theme2 = new TextInputFile(strSelectedTheme+".txt");
+		
 		//setting the number of arrays for strword to be the number of words in the themes file
-		strword = new String[intthemenum+1];
+		strword = new String[intthemenum+1][2];
+		
 		//putting each word into the array
 		for(intcount = 0; intcount<=intthemenum; intcount++){
-			strword[intcount] = theme2.readLine();
-			intrand[intcount] = Math.random();
-			con.println(intcount+" "+strword[intcount]+" random: "+intrand);
+			strword[intcount][0] = theme2.readLine();
+			strword[intcount][1] = Double.toString(Math.random());
+			con.println(intcount+" "+strword[intcount][0]+" random: "+strword[intcount][1]);
 		}
-		//assigning each word a random number
-		
+		//bubble sort
+		double dblsortnum;
+			for(intcount = 0; intcount<intthemenum-1; intcount++){
+				if(Integer.parseInt(strword[intcount][1])>Integer.parseInt(strword[intcount+1][1])){
+					dblsortnum = strword[intcount][1];
+					strword[intcount][1] = strword[intcount+1][1];
+					strword[intcount+1][1] = dblsortnum;
+				}
+			}
 	}
 }
