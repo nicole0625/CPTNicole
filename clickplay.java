6287836ca398a8b4af9dfdb70eb8fee54c41fbd2
Nicole <143809859+nicole0625@users.noreply.github.com>
2025-06-03 11:03:14 -0400

    Update clickplay.java

--- clickplay.java
+++ clickplay.java
@@ -3,11 +3,14 @@ import java.awt.Color;
 import java.lang.Object;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.Font;
 
 public class clickplay{
 	public static void main(String[] args){
 		Console con = new Console("PLAY START");
-		con.setBackgroundColor(new Color(40,36,92));
+		con.setBackgroundColor(new Color(209,113,142));
+		Font fntTest = con.loadFont("TimesRoman.ttf", 40);
+		
 		String strname;
 		String strSelectedTheme;
 		con.println("Please enter your name:");
@@ -84,12 +87,13 @@ public class clickplay{
 		String strguess;
 		int intwrongs;
 		int intrannum;
-		String strranlet;
 		System.out.println("SECRET WORD: "+strword[0][0]);
 		intnumofletters = strword[0][0].length();
 		intwrongs = 0;
 		String strletters[][];
-		//strletters = new String[intnumofletters][1];
+		String strhidden[][];
+		strletters = new String[intnumofletters][2];
+		strhidden =new String[intnumofletters][1];
 		//drawing hanging pole
 		con.setDrawColor(new Color(255,194,240));
 		con.drawLine(680, 140, 790, 140);
@@ -97,18 +101,22 @@ public class clickplay{
 		con.drawLine(680, 140, 680, 400);
 		con.drawLine(650, 400, 830, 400);
 	
-		for(intcount = 0; intcount< intnumofletters; intcount++){
-			con.print("_ ");
-		}
-		
-		/*for(intcount = 0; intcount< intnumofletters; intcount++){
-			strletters[intcount][1] = strword[0][0].substring(intcount, intcount+1);
-			System.out.println("Letters in word: "+strletters);
-	
-		}*/
 			
+		con.clear();
+		//setting up letters of hidden word in an array to randomly select one to reveal later
+		for(intcount = 0; intcount<intnumofletters; intcount++){
+			strletters[intcount][0]=strword[0][0].substring(intcount,intcount+1);
+			strletters[intcount][1] = Integer.toString(intcount);
+			System.out.println(strletters[intcount][0]+" || "+strletters[intcount][1]);
+		}
+		//hidden word
+		for(intcount = 0;intcount<intnumofletters; intcount++){
+			strhidden[intcount][0]="_";
+			con.print(strhidden[intcount][0]);
+		}
 		con.println("");
-		con.print("Type in your guess: ");
+		con.println("Type in your guess: ");
+		
 		strguess = con.readLine();
 		while(!strguess.equalsIgnoreCase(strword[0][0]) && intwrongs <5){
 			//con.clear();
@@ -124,22 +132,32 @@ public class clickplay{
 			}else if(intwrongs == 5){
 				con.drawLine(788, 292, 772, 350); // leg left
 			}
-			intrannum = (int)(Math.random()*intnumofletters)-1;
-			strranlet = strword[0][0].substring(intrannum, intrannum+1);
-			con.println("Random letter: "+strranlet);
+		
+			con.clear();
+			//selecting random letter in word to reveal letter
+			intrannum = ((int)(Math.random()*intnumofletters));
+			System.out.println("Random numer: "+intrannum);
+			strhidden[intrannum][0] = strword[0][0].substring(intrannum,intrannum+1);
+			//printing new line with another letter revealed
+			for(intcount = 0;intcount<intnumofletters; intcount++){
+				con.print(strhidden[intcount][0]);
+			}
+			con.println(" ");
 			con.println("Wrong! Guess again");
 			strguess = con.readLine();
-			intrannum = (int)(Math.random()*intnumofletters)-1;
+
 		}
 		if(intwrongs ==5){
 			con.drawLine(792, 292, 808, 350); // leg right
 			con.clear();
+			con.setBackgroundColor(new Color(133,21,21));
 			con.println("GAME OVER");
 		}
 		
 		
 		if(strguess.equalsIgnoreCase(strword[0][0])){
 			con.clear();
+			con.setBackgroundColor(new Color(89,161,39));
 			con.println("Congrats! You got the secret word, "+strword[0][0]);
 		}
 	
